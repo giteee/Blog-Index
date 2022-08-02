4cc0a8bbcbdc969fef7257743dca82a2031a6424
giteee <90543412+giteee@users.noreply.github.com>
2022-08-02 18:01:32 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,18 +1,17 @@
 const SERVE = false; // 是否使用线上服务器
 
-const HOST = "https://esunr.xyz:8080/api/esunrIndex"; // 线上服务器地址，如果不使用请留空
+const HOST = ""; // 线上服务器地址，如果不使用请留空
 
-const BLOG_NAME = "我的博客"; // 个人网站名字
+const BLOG_NAME = "VPS.VG"; // 个人网站名字
 
-const BLOG_URL = "https://blog.esunr.xyz"; // 个人网站链接（请填写完整链接）
+const BLOG_URL = "//VPS.VGVPS.VG"; // 个人网站链接（请填写完整链接）
 
-const GITHUB = "https://github.com/EsunR"; // 右上角 Github 图标链接，留空不显示
+const GITHUB = "//VPS.VG"; // 右上角 Github 图标链接，留空不显示
 
 const BACKGROUND_IMG_URL = "https://api.dujin.org/bing/1920.php"; // 背景图片url，默认为必应每日图片（第三方接口）
 
 const SORT_DATA = [
-  { sortId: 1, title: "示例分类1" },
-  { sortId: 2, title: "示例分类2" }
+  { sortId: 1, title: "Hi" },
 ]; // 网站分类列表，请按照API.md中'/getSort'接口中返回的data格式填写
 
 const RECORD_NUMBER = ""; // 网站备案号，留空不显示
@@ -21,17 +20,9 @@ const PAGES_DATA = [
   {
     pageId: 1,
     sortId: 1,
-    title: "主标题",
-    subtitle: "副标题",
-    url: "http://www.YourWebPage.com/",
-    icon: ""
-  },
-  {
-    pageId: 2,
-    sortId: 2,
-    title: "主标题",
-    subtitle: "副标题",
-    url: "http://www.YourWebPage.xyz/",
+    title: "VPS.VG",
+    subtitle: " ",
+    url: "//VPS.VG",
     icon: ""
   }
 ]; // 网站分类列表，请按照API.md中'/getPages'接口中返回的data格式填写
